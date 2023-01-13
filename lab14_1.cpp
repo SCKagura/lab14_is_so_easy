cb377c89745c2478b8fe625d215b4038f69618ae
= <=>
2023-01-13 17:24:00 +0700

    lab_14_full_complete

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,37 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+
+
+void stat(const double A[],int N,double B[] ) 
+{
+    double Max=0,Min=100000,sum=0,average=0,SDsum=0,GMSum=1,HMsum=0 ;
+
+    for( int i = 0; i<6 ;i++ ) 
+        {
+            sum += A[i] ;  //Arithmetic Mean
+            SDsum += pow(A[i],2) ; //Standard Deviation
+            GMSum *= A[i] ;    //Geometric Mean
+            HMsum += 1.0/A[i] ;  //Harmonic Mean
+            if(Max<A[i])
+            {
+                Max = A[i] ;
+            }
+            if(Min>A[i]) 
+            {
+                Min = A[i] ;
+            }
+        }
+    average = sum/N ;
+    B[0] = average ; 
+    B[1] = sqrt((SDsum/N)-pow(average,2)) ;
+    B[2] = pow(GMSum,1.0/N) ;
+    B[3] = N/HMsum ;
+    B[4] = Max ;
+    B[5] = Min ;
+
+
+
+}
+
